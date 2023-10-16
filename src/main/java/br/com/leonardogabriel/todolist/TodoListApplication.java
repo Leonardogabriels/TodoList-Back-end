1bb25d15b7201809247eb20cf5479e7e03ed97c3
leonardogabriels <leonardogabrielsilva1@outlook.com>
2023-10-16 12:45:57 -0300

    integração com bamco de dados

--- src/main/java/br/com/leonardogabriel/todolist/TodoListApplication.java
+++ src/main/java/br/com/leonardogabriel/todolist/TodoListApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciadorDeTarefasApplication {
+public class TodoListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorDeTarefasApplication.class, args);
+		SpringApplication.run(TodoListApplication.class, args);
 	}
 
 }
